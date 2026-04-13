19b8c86613e222373ba77761c5625d62803bb389
Raphael Muniz <raphael@copin.ufcg.edu.br>
2026-04-13 10:07:27 -0300

    Adicionar diretório de arquivos estáticos ao settings.py

--- Projeto/mercado/settings.py
+++ Projeto/mercado/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'produtos',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']

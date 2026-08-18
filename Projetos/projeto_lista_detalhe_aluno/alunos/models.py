2f1cd243ac6d31fbd364ea454458e001d599406a
Raphael Muniz <raphael@copin.ufcg.edu.br>
2026-08-18 14:17:33 -0300

    Incluindo modelo de aluno e alterando cores do header e footer.

--- Projetos/projeto_lista_detalhe_aluno/alunos/models.py
+++ Projetos/projeto_lista_detalhe_aluno/alunos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+   nome = models.CharField(max_length=100)
+   idade = models.IntegerField()
+   email = models.EmailField()
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+       return self.nome

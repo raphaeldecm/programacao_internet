43f5a172a8da172a2662ec9a5f0a6345f92c53d5
Raphael Muniz <raphael@copin.ufcg.edu.br>
2026-09-01 20:19:49 -0300

    feat: atualizar estrutura e estilos do portal escolar com novos modelos e navegação

--- Projetos/projeto_portal_escolar/noticias/models.py
+++ Projetos/projeto_portal_escolar/noticias/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Tag(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.titulo
